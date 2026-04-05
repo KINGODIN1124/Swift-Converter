246e183a213d10ec544f981d3644d740463ac336
SwiftConvert Developer <developer@swiftconvert.com>
2026-04-05 10:43:41 +0530

    Fix: Disable autoUpdate to solve initial load freeze

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,7 @@ const config: CapacitorConfig = {
   webDir: 'out',
   plugins: {
     CapacitorUpdater: {
-      autoUpdate: true,
+      autoUpdate: false,
       statsUrl: 'https://capgo.app/api/stats/',
     },
   },
